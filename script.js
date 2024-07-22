8f3e06f8cf743c531ae28de5fdc7e31fadd1dba2
Subish Kumar A <156400422+subish31@users.noreply.github.com>
2024-07-22 17:10:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function ryt(){
+    document.getElementById('op4').style.backgroundColor="green";
+    document.getElementById('op44').style.backgroundColor="green";
+}
+function wrng1(){
+    document.getElementById('op1').style.backgroundColor="red";
+    document.getElementById('op11').style.backgroundColor="red";
+}
+function wrng2(){
+    document.getElementById('op2').style.backgroundColor="red";
+    document.getElementById('op22').style.backgroundColor="red";
+}
+function wrng3(){
+    document.getElementById('op3').style.backgroundColor="red";
+    document.getElementById('op33').style.backgroundColor="red";
+}
